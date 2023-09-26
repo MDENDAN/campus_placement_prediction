0eaf8fe025e602d399eb0ca1449c2c51aacb901d
MDENDAN <md.mollick15697@sakec.ac.in>
2023-09-26 18:33:55 +0530

    directory errors

--- app.py
+++ app.py
@@ -11,10 +11,10 @@ print("Current Directory:", current_directory)
 
 # Load your trained model (make sure the model file 'svm_model_OPT.pkl' is in the same directory)
 # status_model_filename = 'model\status\svm_model_OPT.pkl'
-status_model = load_model('model\status\DT_model.joblib')
+status_model = load_model(r'.\\model\status\DT_model.joblib')
 
 # salary_model_filename = 'model/salary/random_forest_regression_model.joblib'
-salary_model = load_model('model\salary\\random_forest_regression_model.joblib')
+salary_model = load_model(r'.\\model\salary\\random_forest_regression_model.joblib')
 
 # Create a Streamlit web app
 st.title('Campus Recruitment Prediction')
